6a409116e91a80ecaf9252bccc052f44cc1f05b8
Boluwatife <boluwatife8081@gmail.com>
2025-02-03 19:29:45 +0100

    updated albemic

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:Ilerioluwa1%40@localhost/ecommerce_db"
+DATABASE_URL = "postgresql://ecommerce:ecommerce@localhost/ecommerce_db"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)

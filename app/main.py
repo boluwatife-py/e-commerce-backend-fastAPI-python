1831179538d61dd73450771fb2498795c75efcef
Boluwatife <boluwatife8081@gmail.com>
2025-02-04 22:47:29 +0100

    files updated, server running

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from app.routes import users
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="E-Commerce API")
 app.include_router(users.router, prefix="/api")
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,23 +1,23 @@
-# from fastapi import APIRouter, Depends, HTTPException
-# from sqlalchemy.orm import Session
-# from app.database import SessionLocal
-# from app.crud import create_user, get_user_by_email
-# from app.schemas import UserCreate, UserResponse
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from app.database import SessionLocal
+from app.crud import create_user, get_user_by_email
+from app.schemas import UserCreate, UserResponse
 
-# router = APIRouter()
+router = APIRouter()
 
 
-# def get_db():
-#     db = SessionLocal()
-#     try:
-#         yield db
-#     finally:
-#         db.close()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
-# @router.post("/users", response_model=UserResponse)
-# def register_user(user: UserCreate, db: Session = Depends(get_db)):
-#     db_user = get_user_by_email(db, user.email)
-#     if db_user:
-#         raise HTTPException(status_code=400, detail="Email already registered")
+@router.post("/users", response_model=UserResponse)
+def register_user(user: UserCreate, db: Session = Depends(get_db)):
+    db_user = get_user_by_email(db, user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
     
-#     return create_user(db, user)
+    return create_user(db, user)
